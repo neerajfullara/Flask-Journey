eba7dc1ed9c66d6727bcff46a660c89cefffd64b
neerajfullara <fullaraneeraj@gmail.com>
2025-05-29 20:37:54 +0530

    Day-1 of Flask. Learned 'How to setup the project' and some basic routing methods.

--- Day-1/app.py
+++ Day-1/app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello World</h1>"
+
+@app.route("/home")
+def home():
+    return "<h1>This the home.</h1?"
+
+@app.route("/json")
+def json():
+    return {
+        "My Key": "JSON Value"
+    }
